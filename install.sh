c9b2f47a179492b64c931baddfdda360c1f1bb4f
JsjaiizjsidisizkkXxd <142564837+ofccccxd8@users.noreply.github.com>
2024-08-15 11:39:51 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -30,7 +30,7 @@ set -e
 
 export GITHUB_SOURCE="v1.1.0"
 export SCRIPT_RELEASE="v1.1.0"
-export GITHUB_BASE_URL="https://raw.githubusercontent.com/pterodactyl-installer/pterodactyl-installer"
+export GITHUB_BASE_URL="https://raw.githubusercontent.com/ofccccxd8/pterodactyl-installer"
 
 LOG_PATH="/var/log/pterodactyl-installer.log"
 
